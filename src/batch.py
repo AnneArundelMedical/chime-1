daa58045a22e975f4f6a13caea832961e245f4b4
Nate C-K <nculwell@gmail.com>
2020-04-02 12:42:08 -0500

    Fixes.

--- src/batch.py
+++ src/batch.py
@@ -53,16 +53,17 @@ base_params = {
     "relative_contact_rate": .30,
 }
 
-def generate_param_permutations(base_params, regions, doubling_times, relative_contact_rates):
+def generate_param_permutations(base_params, current_date, regions, doubling_times, relative_contact_rates):
     for region in regions:
         for dt in doubling_times:
             for rcr in relative_contact_rates:
-                p = combine_params(base_params, region, dt, rcr)
+                p = combine_params(base_params, current_date, region, dt, rcr)
                 yield p
 
-def combine_params(base_params, region, doubling_time, relative_contact_rate):
+def combine_params(base_params, current_date, region, doubling_time, relative_contact_rate):
     p = dict(base_params)
     p.update(region)
+    p["current_date"] = current_date
     p["doubling_time"] = doubling_time
     p["relative_contact_rate"] = relative_contact_rate
     return p
@@ -70,94 +71,89 @@ def combine_params(base_params, region, doubling_time, relative_contact_rate):
 def load_hospital_census_data(report_date):
     print("load_hospital_census_data")
     data_path = input_file_path(report_date)
-    """
-    #date_parser = lambda dt: pd.datetime.strptime(dt, "%Y-%M-%D %H:%M:%S.%U")
-    census_df = pd.read_csv(data_path, parse_dates=True) # , date_parser=date_parser)
-    census_df = pd.read_csv(data_path, dtype=pd.StringDtype())
-    """
     census_df = pd.read_csv(data_path)
-    """
-    census_df = pd.read_csv(data_path, index_col=0, dtype={
-        HOSP_DATA_COLNAME_DATE: pd.StringDtype,
-        HOSP_DATA_COLNAME_TESTRESULT: pd.StringDtype,
-        HOSP_DATA_COLNAME_TOTAL_PATS: pd.StringDtype, # pd.Int32Dtype,
-        })
-    """
-    print(census_df)
-    print(census_df.dtypes)
+    #print(census_df)
     datetime_column = pd.to_datetime(census_df[HOSP_DATA_COLNAME_DATE], format="%Y-%m-%d %H:%M:%S")
-    print(datetime_column)
+    #print(datetime_column)
     census_df[HOSP_DATA_COLNAME_TRUE_DATETIME] = datetime_column
-    print(census_df)
-    #d = census_df[HOSP_DATA_COLNAME_DATE].str.rstrip("0").rstrip(".").to_datetime()
-    #census_df[HOSP_DATA_COLNAME_DATE] = census_df[HOSP_DATA_COLNAME_DATE].str.rstrip("0").rstrip(".").to_datetime()
-    """
-    census_df = pd.read_csv(data_path).astype({
-        HOSP_DATA_COLNAME_DATE: "datetime64",
-        HOSP_DATA_COLNAME_TESTRESULT: pd.StringDtype(),
-        HOSP_DATA_COLNAME_TOTAL_PATS: "int32"
-    })
-    """
+    #print(census_df)
     is_positive = census_df[HOSP_DATA_COLNAME_TESTRESULT] == "POSITIVE"
     positive_df = census_df[is_positive]
     grouped_df = positive_df.set_index(HOSP_DATA_COLNAME_TRUE_DATETIME).resample("D")
-    max_pos_df = grouped_df[HOSP_DATA_COLNAME_TOTAL_PATS].max()
-    return max_pos_df
+    max_pats_series = grouped_df[HOSP_DATA_COLNAME_TOTAL_PATS].max()
+    print("MAX PATS DF")
+    print(max_pats_series)
+    hosp_census_today = max_pats_series.filter([report_date])[0]
+    print("Patients on report date: %s" % hosp_census_today)
+    print("to_frame")
+    max_pats_df = max_pats_series.to_frame()
+    print(max_pats_df)
+    #print("FINAL")
+    #print(max_pats_series.iloc[:,[0]])
+    #print("/FINAL")
+    return max_pats_df, hosp_census_today
 
-def original_variations():
+def original_variations(today):
     """This is the original report that I did on the first day."""
     doubling_times = [ 3.0, 3.5, 4.0 ]
     relative_contact_rates = [ .25, .30, .40 ]
     param_set = (base_params, regions, doubling_times, relative_contact_rates)
     write_model_outputs_for_permutations(*param_set)
 
-def data_based_variations():
+def data_based_variations(today):
     print("data_based_variations")
-    today = date.today()
-    hosp_census_df = load_hospital_census_data(today)
+    hosp_census_df, patients_today = load_hospital_census_data(today)
     print("LOAD COMPLETE")
     print(hosp_census_df)
     print(hosp_census_df.dtypes)
-    hosp_census_today_df = hosp_census_df.filter([today])
-    print(hosp_census_today_df)
-    patients_today = hosp_census_today_df[0]
     print("Patients today: %s" % patients_today)
     base = dict(base_params)
     base["current_hospitalized"] = patients_today
     doubling_times = ( dt/10.0 for dt in range(28, 41) )
     relative_contact_rates = ( rcr/100.0 for rcr in range(20, 51) )
-    param_set = (base_params, regions, doubling_times, relative_contact_rates)
-    write_model_outputs_for_permutations(*param_set)
+    param_set = (base_params, today, regions, doubling_times, relative_contact_rates)
+    #write_model_outputs_for_permutations(*param_set)
     find_best_fitting_params(hosp_census_df, *param_set)
 
 def write_model_outputs_for_permutations(
-            base_params, regions, doubling_times, relative_contact_rates):
+            base_params, current_date, regions, doubling_times, relative_contact_rates):
     print("write_model_outputs_for_permutations")
     for p in generate_param_permutations(
-            base_params, regions, doubling_times, relative_contact_rates
+            base_params, current_date, regions, doubling_times, relative_contact_rates
             ):
         write_model_outputs(p)
 
 def find_best_fitting_params(hosp_census_df,
-            base_params, regions, doubling_times, relative_contact_rates):
+            base_params, today, regions, doubling_times, relative_contact_rates):
     print("find_best_fitting_params")
     best_score = 1e10
     best_params = None
-    hosp_dates = hosp_census_df[HOSP_DATA_COLNAME_DATE]
+    hosp_dates = hosp_census_df.index
+    print("hosp_dates\n", hosp_dates)
     for region in regions:
         for p in generate_param_permutations(
-                base_params, [region], doubling_times, relative_contact_rates
+                base_params, today, [region], doubling_times, relative_contact_rates
                 ):
             m = get_model_from_params(p)
-            census_df = m.census_df
-            matched_pred_census_df = census_df[census_df[PENNMODEL_COLNAME_DATE].isin(hosp_dates)]
+            census_df = m.census_df.dropna()
+            census_df = census_df.set_index(PENNMODEL_COLNAME_DATE);
+            census_df.to_csv("census.csv")
+            #print("census_df\n", census_df)
+            #print("census_df.dtypes\n", census_df.dtypes)
+            dates_intersection = census_df.index.intersection(hosp_dates)
+            matched_pred_census_df = census_df.loc[dates_intersection]
+            #print("matched_pred_census_df\n", matched_pred_census_df)
+            matched_hosp_census_df = hosp_census_df.loc[dates_intersection]
+            #print("matched_hosp_census_df\n", matched_hosp_census_df)
             mse = mean_squared_error(
-                hosp_census_df[HOSP_DATA_COLNAME_TOTAL_PATS], 
+                matched_hosp_census_df[HOSP_DATA_COLNAME_TOTAL_PATS], 
                 matched_pred_census_df[PENNMODEL_COLNAME_HOSPITALIZED]
                 )
             if mse < best_score:
                 best_score = mse
                 best_params = p
+                print("*" * 60)
+                print("BEST SCORE: %g" % mse)
         write_model_outputs(best_params, "Best")
         params_filename = "PennModel_%s_%s_bestparams.json" % (date.today().isoformat(), region)
         with open(params_filename, "w") as f:
@@ -173,8 +169,7 @@ def get_model_from_params(parameters):
     m = SimSirModel(params_obj)
     return m
 
-def write_model_outputs(parameters, filename_annotation = None):
-    today = date.today()
+def write_model_outputs(today, parameters, filename_annotation = None):
     p = parameters
     m = get_model_from_params(p)
     charts = [
@@ -195,7 +190,11 @@ def write_model_outputs(parameters, filename_annotation = None):
         df.to_csv(path)
 
 if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        today_override = date.fromisoformat(sys.argv[1])
+    else:
+        today_override = date.today()
     print("Pandas version:", pd.__version__)
     #original_variations()
-    data_based_variations()
+    data_based_variations(today_override)
 
