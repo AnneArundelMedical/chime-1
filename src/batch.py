9e8264847f0d3db89c644e9e5e7e0b7aca88819b
Nate C-K <nculwell@gmail.com>
2020-04-09 08:44:49 -0500

    Fix duplicates. Write output to single file.

--- src/batch.py
+++ src/batch.py
@@ -64,14 +64,14 @@ def output_file_path(main_label, sub_label, chart_name, parameters):
 
 # Population from www.census.gov, 2019-07-01 estimate (V2019).
 # Example link: https://www.census.gov/quickfacts/annearundelcountymaryland
-regions = [
+REGIONS = [
     { "region_name": "Anne Arundel", "population": 597234, "market_share": .30, },
     { "region_name": "Prince George's", "population": 909327, "market_share": .07, },
     { "region_name": "Queen Anne's", "population": 50381, "market_share": .40, },
     { "region_name": "Talbot", "population": 37181, "market_share": .09, },
 ];
 
-base_params = {
+BASE_PARAMS = {
     "current_hospitalized": 14,
     # rates for whole pop
     "hospitalized": Disposition(.044, 10),
@@ -93,30 +93,110 @@ base_params = {
 
 ITERATION_COUNT = 0
 
+def parsedate(ds):
+    return datetime.date.fromisoformat(ds)
+
 def generate_param_permutations(
         base_params, current_date, regions, doubling_times,
         relative_contact_rates, mitigation_dates
         ):
-    perms = []
+    params = []
     for region in regions:
         for dt in doubling_times:
             for rcr in relative_contact_rates:
                 for md in mitigation_dates:
                     p = combine_params(base_params, current_date, region, dt, rcr, md)
-                    perms.append(p)
-    return perms
+                    params.append(p)
+    return params
 
 def combine_params(base_params, current_date, region, doubling_time,
         relative_contact_rate, mitigation_date):
-    p = dict(base_params)
-    p.update(region)
+    p = { **base_params, **region }
     p["current_date"] = current_date
-    #print(region)
-    p["region"] = Regions(**{ region["region_name"]: region["population"] })
     p["doubling_time"] = doubling_time
     p["relative_contact_rate"] = relative_contact_rate
     p["mitigation_date"] = mitigation_date
     return p
+
+def test_params():
+    TEST_DATE = parsedate("2020-02-01")
+    base_params = {
+        "current_hospitalized": 14,
+        "hospitalized": Disposition(.044, 10),
+        "relative_contact_rate": .30,
+        "current_date": datetime.date.today(),
+        "doubling_time": 3.0,
+        "relative_contact_rate": .30,
+    }
+    current_date = TEST_DATE
+    regions = [ REGIONS[0] ]
+    doubling_times = [ 3.0, 4.0 ]
+    relative_contact_rates = [ .30, .40 ]
+    mitigation_dates = [ parsedate(d) for d in ['2020-02-05', '2020-02-10'] ]
+    params = generate_param_permutations(
+        base_params, current_date, regions, doubling_times,
+        relative_contact_rates, mitigation_dates
+    )
+
+    print(params)
+
+    assert len(params) == 8
+
+    expected = [
+        { "current_date": TEST_DATE, **REGIONS[0], "region": REGIONS[0],
+          "hospitalized": Disposition(.044, 10), "current_hospitalized": 14,
+          "mitigation_date": parsedate("2020-02-05"),
+          "doubling_time": 3.0, "relative_contact_rate": .30, },
+        { "current_date": TEST_DATE, **REGIONS[0], "region": REGIONS[0],
+          "hospitalized": Disposition(.044, 10), "current_hospitalized": 14,
+          "mitigation_date": parsedate("2020-02-05"),
+          "doubling_time": 4.0, "relative_contact_rate": .30, },
+        { "current_date": TEST_DATE, **REGIONS[0], "region": REGIONS[0],
+          "hospitalized": Disposition(.044, 10), "current_hospitalized": 14,
+          "mitigation_date": parsedate("2020-02-05"),
+          "doubling_time": 3.0, "relative_contact_rate": .40, },
+        { "current_date": TEST_DATE, **REGIONS[0], "region": REGIONS[0],
+          "hospitalized": Disposition(.044, 10), "current_hospitalized": 14,
+          "mitigation_date": parsedate("2020-02-05"),
+          "doubling_time": 4.0, "relative_contact_rate": .40, },
+        { "current_date": TEST_DATE, **REGIONS[0], "region": REGIONS[0],
+          "hospitalized": Disposition(.044, 10), "current_hospitalized": 14,
+          "mitigation_date": parsedate("2020-02-10"),
+          "doubling_time": 3.0, "relative_contact_rate": .30, },
+        { "current_date": TEST_DATE, **REGIONS[0], "region": REGIONS[0],
+          "hospitalized": Disposition(.044, 10), "current_hospitalized": 14,
+          "mitigation_date": parsedate("2020-02-10"),
+          "doubling_time": 4.0, "relative_contact_rate": .30, },
+        { "current_date": TEST_DATE, **REGIONS[0], "region": REGIONS[0],
+          "hospitalized": Disposition(.044, 10), "current_hospitalized": 14,
+          "mitigation_date": parsedate("2020-02-10"),
+          "doubling_time": 3.0, "relative_contact_rate": .40, },
+        { "current_date": TEST_DATE, **REGIONS[0], "region": REGIONS[0],
+          "hospitalized": Disposition(.044, 10), "current_hospitalized": 14,
+          "mitigation_date": parsedate("2020-02-10"),
+          "doubling_time": 4.0, "relative_contact_rate": .40, },
+    ]
+
+    assert len(expected) == len(params)
+    assert lists_equal(expected, expected)
+    assert lists_equal(expected, params)
+
+def lists_equal(a, b):
+    if len(a) != len(b):
+        return False
+    a = list(a)
+    b = list(b)
+    for ai in range(len(a)):
+        for bi in range(len(b)):
+            print(ai, bi)
+            ax = a[ai]
+            bx = b[bi]
+            if a[ai] == b[bi]:
+                del b[bi]
+                break
+        else:
+            raise ValueError("Unmatched index: %d" % ai)
+    return True
 
 def load_hospital_census_data(report_date):
     print("load_hospital_census_data")
@@ -151,7 +231,7 @@ def original_variations(day):
     """This is the original report that I did on the first day."""
     doubling_times = [ 3.0, 3.5, 4.0 ]
     relative_contact_rates = [ .25, .30, .40 ]
-    param_set = (base_params, regions, doubling_times, relative_contact_rates)
+    param_set = (BASE_PARAMS, REGIONS, doubling_times, relative_contact_rates)
     write_model_outputs_for_permutations(*param_set)
 
 def data_based_variations(day):
@@ -161,13 +241,13 @@ def data_based_variations(day):
     print(hosp_census_df)
     print(hosp_census_df.dtypes)
     print("Patients today: %s" % patients_today)
-    base = dict(base_params)
+    base = dict(BASE_PARAMS)
     base["current_hospitalized"] = patients_today
     doubling_times = VARYING_PARAMS["doubling_time"]
     relative_contact_rates = VARYING_PARAMS["relative_contact_rate"]
     mitigation_dates = VARYING_PARAMS["mitigation_date"]
     param_set = (
-            base_params, day, regions, doubling_times,
+            BASE_PARAMS, day, REGIONS, doubling_times,
             relative_contact_rates, mitigation_dates
             )
     #write_model_outputs_for_permutations(*param_set)
@@ -197,50 +277,58 @@ def find_best_fitting_params(hosp_census_df,
     #print(list(doubling_times))
     #print(list(relative_contact_rates))
     #print(list(generate_param_permutations(base_params, day, regions, doubling_times, relative_contact_rates)))
-    for p in generate_param_permutations(
-            base_params, day, regions, list(doubling_times),
-            list(relative_contact_rates), mitigation_dates
-            ):
-        region_name = p["region_name"]
-        m = get_model_from_params(p)
-        census_df = m.census_df
-        #print("census_df\n", census_df)
-        census_df.to_csv(os.path.join(OUTPUT_DIR, "debug-model-census-dump.csv"))
-        census_df = census_df.dropna() # remove rows with NaN values
-        census_df = census_df.set_index(PENNMODEL_COLNAME_DATE);
-        #print("census_df\n", census_df)
-        #print("census_df.dtypes\n", census_df.dtypes)
-        dates_intersection = census_df.index.intersection(hosp_dates)
-        matched_pred_census_df = census_df.loc[dates_intersection]
-        #print("matched_pred_census_df\n", matched_pred_census_df)
-        matched_hosp_census_df = hosp_census_df.loc[dates_intersection]
-        #print("matched_hosp_census_df\n", matched_hosp_census_df)
-        #print("mse")
-        #print(matched_hosp_census_df[HOSP_DATA_COLNAME_TOTAL_PATS])
-        #print(matched_pred_census_df[PENNMODEL_COLNAME_HOSPITALIZED])
-        mse = mean_squared_error(
-            matched_hosp_census_df[HOSP_DATA_COLNAME_TOTAL_PATS], 
-            matched_pred_census_df[PENNMODEL_COLNAME_HOSPITALIZED]
-            )
-        if mse < best[region_name]["score"]:
-            best[region_name]["score"] = mse
-            best[region_name]["params"] = p
-            #print("*" * 60)
-            #print("BEST SCORE: %g" % mse)
-        write_fit_rows(p, m.census_df, mse)
-    try:
-        print("BEST PARAMS:")
-        for (region_name, b) in best.items():
-            print("Best params for region:", region_name)
-            print(b["params"])
-    except Exception as e:
-        print("(printing error: %s)" % str(e))
-    #write_model_outputs(best_params, "Best")
-    #params_filename = "PennModel_%s_%s_bestparams.json" % (day.isoformat(), region)
-    #with open(params_filename, "w") as f:
-    #    json.dump({ "params": p, "mse": best_score }, f)
+    params_list = generate_param_permutations(
+        base_params, day, regions, list(doubling_times),
+        list(relative_contact_rates), mitigation_dates
+        )
+    with open("PARAMS.txt", "w") as f:
+        for p in params_list:
+            print(p, file=f)
+    is_first_batch = True
+    output_file_path = os.path.join(OUTPUT_DIR, "PennModelFit_Combined_%s.csv" % day.isoformat())
+    with open(output_file_path, "w") as output_file:
+        for p in params_list:
+            region_name = p["region_name"]
+            m = get_model_from_params(p)
+            census_df = m.census_df
+            #print("census_df\n", census_df)
+            census_df.to_csv(os.path.join(OUTPUT_DIR, "debug-model-census-dump.csv"))
+            census_df = census_df.dropna() # remove rows with NaN values
+            census_df = census_df.set_index(PENNMODEL_COLNAME_DATE);
+            #print("census_df\n", census_df)
+            #print("census_df.dtypes\n", census_df.dtypes)
+            dates_intersection = census_df.index.intersection(hosp_dates)
+            matched_pred_census_df = census_df.loc[dates_intersection]
+            #print("matched_pred_census_df\n", matched_pred_census_df)
+            matched_hosp_census_df = hosp_census_df.loc[dates_intersection]
+            #print("matched_hosp_census_df\n", matched_hosp_census_df)
+            #print("mse")
+            #print(matched_hosp_census_df[HOSP_DATA_COLNAME_TOTAL_PATS])
+            #print(matched_pred_census_df[PENNMODEL_COLNAME_HOSPITALIZED])
+            mse = mean_squared_error(
+                matched_hosp_census_df[HOSP_DATA_COLNAME_TOTAL_PATS], 
+                matched_pred_census_df[PENNMODEL_COLNAME_HOSPITALIZED]
+                )
+            if mse < best[region_name]["score"]:
+                best[region_name]["score"] = mse
+                best[region_name]["params"] = p
+                #print("*" * 60)
+                #print("BEST SCORE: %g" % mse)
+            write_fit_rows(p, m.census_df, mse, is_first_batch, output_file)
+            is_first_batch = False
+        try:
+            print("BEST PARAMS:")
+            for (region_name, b) in best.items():
+                print("Best params for region:", region_name)
+                print(b["params"])
+        except Exception as e:
+            print("(printing error: %s)" % str(e))
+        #write_model_outputs(best_params, "Best")
+        #params_filename = "PennModel_%s_%s_bestparams.json" % (day.isoformat(), region)
+        #with open(params_filename, "w") as f:
+        #    json.dump({ "params": p, "mse": best_score }, f)
 
-def write_fit_rows(p, census_df, mse):
+def write_fit_rows(p, census_df, mse, is_first_batch, output_file):
     df = census_df.dropna().set_index(PENNMODEL_COLNAME_DATE)
     df["region_name"] = p["region_name"]
     df["population"] = p["population"]
@@ -258,15 +346,18 @@ def write_fit_rows(p, census_df, mse):
     df["ventilated_rate"] = p["ventilated"].rate
     df["ventilated_days"] = p["ventilated"].days
     """
-    path = output_file_path("PennModelFit", None, "census", p)
-    df.to_csv(path)
+    #path = output_file_path("PennModelFit", None, "census", p)
+    df.to_csv(output_file, header=is_first_batch)
+
+#{'current_hospitalized': 14, 'hospitalized': Disposition(rate=0.044, days=10), 'icu': Disposition(rate=0.013, days=10), 'relative_contact_rate': 0.2, 'ventilated': Disposition(rate=0.007, days=10), 'current_date': datetime.date(2020, 4, 8), 'doubling_time': 2.8, 'infectious_days': 14, 'n_days': 90, 'recovered': 0, 'region_name': 'Anne Arundel', 'population': 597234, 'market_share': 0.3, 'mitigation_date': datetime.date(2020, 3, 1)}
 
 def rounded_percent(pct):
     return int(100 * pct)
 
 def get_model_from_params(parameters):
     #print("PARAMETERS PRINT", parameters)
-    p = dict(parameters)
+    p = { **parameters }
+    p["region"] = Regions(**{ p["region_name"]: p["population"] })
     del p["region_name"]
     params_obj = Parameters(**p)
     m = penn_chime.models.SimSirModel(params_obj)
