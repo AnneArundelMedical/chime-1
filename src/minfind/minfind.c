b28154631d09307f2ed03b35b135d7d6318b53a4
Nate C-K <nculwell@gmail.com>
2020-05-19 01:01:49 -0500

    Working on minfind.

--- src/minfind/minfind.c
+++ src/minfind/minfind.c
@@ -3,67 +3,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <errno.h>
+#include <string.h>
 
-static const char* USAGE = "USAGE: minfind <filename>"
-
-int main(int argc, char** argv) {
-  if (argc != 2) {
-    fprintf(stderr, "Invalid command-line arguments.\n");
-    fprintf(stderr, "%s\n", USAGE);
-    return 1;
-  }
-  minfind(argv[1]);
-}
-
-#define MAX_HEADER_COUNT 1024
+#define MAX_COLS_COUNT 1024
 #define LINE_BUFFER_SIZE 4096
 
-static char* headers[MAX_HEADER_COUNT];
-static char headers_line[LINE_BUFFER_SIZE];
-static char data_line[LINE_BUFFER_SIZE];
+static const char* USAGE = "USAGE: minfind <filename>";
 
-void minfind(const char* path)
+void minfind(const char* path);
+void find_mse_columns(const char* headers, int headers_count,
+    int* h_mse, int* h_mse_icu, int* h_mse_cum);
+
+bool readline(FILE* f, char* buffer, int* len, char** columns, int* columns_count)
 {
-  FILE* csv_file = fopen(path, "r");
-  int headers_line_len;
-  if (!readline(csv_file, headers_line, &headers_line_len)) {
-    fprintf("CSV file has no header line.\n");
-    exit(1);
-  }
-  headers[0] = headers_line;
-  int h = 1;
-  for (int i=0; i < headers_line_len; i++) {
-    if (headers_line[i] == '\0')
-      headers[h++] = &headers_line[i+1];
-  }
-  int headers_count = h;
-  int h_mse, h_mse_icu, h_mse_cum;
-  find_mse_columns(headers, headers_count, &h_mse, &h_mse_icu, &h_mse_cum);
-  find_min_errors(csv_file, 
-}
-
-void find_mse_columns(
-    const char* headers, int headers_count,
-    int* h_mse, int* h_mse_icu, int* h_mse_cum)
-{
-  *h_mse = -1;
-  *h_mse_icu = -1;
-  *h_mse_cum = -1;
-  for (h=0; h < headers_count; h++) {
-    if (!strcmp("mse", headers[h]))
-      *h_mse = h;
-    else if (!strcmp("mse_icu", headers[h]))
-      *h_mse_icu = h;
-    else if (!strcmp("mse_cum", headers[h]))
-      *h_mse_cum = h;
-  }
-  if (*h_mse < 0 || *h_mse_icu < 0 || *h_mse_cum < 0) {
-    fprintf("Didn't find all error columns, aborting.\n");
-    exit(1);
-  }
-}
-
-bool readline(FILE* f, char* buffer, int* len) {
   if (feof(f))
     return false;
   int c;
@@ -86,7 +39,58 @@ bool readline(FILE* f, char* buffer, int* len) {
     exit(1);
   }
   buffer[i] = 0;
-  len = i;
+  *len = i;
+  columns[0] = buffer;
+  int col = 1;
+  for (int i=0; i < *len; i++) {
+    if (buffer[i] == '\0')
+      columns[col++] = &buffer[i+1];
+  }
+  *columns_count = col;
   return true;
+}
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Invalid command-line arguments.\n");
+    fprintf(stderr, "%s\n", USAGE);
+    return 1;
+  }
+  minfind(argv[1]);
+}
+
+static char line[LINE_BUFFER_SIZE];
+static char* cols[MAX_COLS_COUNT];
+static char* column_headers[] = { "mse", "mse_icu", "mse_cum" };
+
+void minfind(const char* path)
+{
+  FILE* csv_file = fopen(path, "r");
+  int line_len, cols_count;
+  if (!readline(csv_file, line, &line_len, cols, &cols_count)) {
+    fprintf(stderr, "CSV file has no header line.\n");
+    exit(1);
+  }
+  int h_mse = find_column(cols, cols_count, "mse");
+  int h_mse_icu = find_column(cols, cols_count, "mse_icu");
+  int h_mse_cum = find_column(cols, cols_count, "mse_cum");
+  double mse_min, mse_icu_min, mse_cum_min;
+  mse_min = mse_icu_min = mse_cum_min = 1e10;
+  int line_number = 2;
+  while (readline(csv_file, line, &line_len, cols, &cols_count)) {
+
+    line_number++;
+  }
+}
+
+int find_column(const char* headers, int headers_count, const char* header_to_find)
+{
+  int found_index = -1;
+  for (h=0; h < headers_count; h++) {
+    if (!strcmp(header_to_find, headers[h]))
+      return h;
+  }
+  fprintf("Didn't find column '%s', aborting.\n", header_to_find);
+  exit(1);
 }
 
